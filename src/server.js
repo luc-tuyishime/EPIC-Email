c59bf4bf8242747e13361898ec07c1131a9cc153
luc <luctunechi45@gmail.com>
2019-02-24 23:05:04 +0200

    [ch-164192398] add node server setup

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { hello } from "./hello/hello";
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+    res.send("Pure JS Version")
+});
+
+hello()
+
+app.listen(process.env.PORT | 3000, () => console.log("Server started..."));
